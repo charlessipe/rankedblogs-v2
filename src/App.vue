e111aee69ea60856fdc78a85d3067bc1701b40d7
Charles Sipe <charlessipe@Charless-MacBook-Pro.local>
2020-05-26 17:31:59 -0700

    update auth state

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,53 @@
 <template>
   <div id="app">
-    <Navigation />
-    <router-view class="container" />
+    <Navigation 
+      :user="user" 
+      @logout="logout"
+    />
+    <router-view 
+      class="container" 
+      :user="user" 
+      @logout="logout"
+    />
   </div>
 </template>
 
 
 <script>
 import Navigation from "@/components/Navigation.vue";
+import Firebase from "firebase";
+import db from "./db.js";  // eslint-disable-line
+
 export default {
-  name: "app",
+  name: "App",
+  data: function() {
+    return {
+      user: null
+    }
+  },
+  methods: {
+    logout: function() {
+      Firebase.auth()
+      .signOut()
+      .then( () => {
+        this.user = null;
+        this.$router.push("login");
+      })
+    }
+  },
+  mounted() {
+    Firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.user = user.displayName + " " + user.email + " " + user.uid;
+      }
+    });
+  },
   components: {
     Navigation
   }
-}
+};
 </script>
+
 
 <style lang="scss">
 @import "node_modules/bootstrap/scss/bootstrap";
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,46 @@
 <template>
   <div>
+    <div 
+      v-if="user"
+      class="text-center"
+    >
+    <h1>Ranked Blogs Vue</h1>
 
-    <h1>Welcome</h1>
+    <p>User name: <strong><span class="text-secondary">{{user}}</span></strong></p>
+    
+    <a href="#" 
+       role="button" 
+       class="text-primary" 
+       @click="$emit('logout')" 
+    >log out</a>
+    <br>
+    <br>
+
+    <router-link
+      class="btn btn-outline-primary mr-2"
+      to="/register"
+      v-if="!user"
+    >Register</router-link>
+    <router-link
+      class="btn btn-outline-primary mr-2"
+      to="/login"
+      v-if="!user"
+    >Log In</router-link>
+    <router-link
+      class="btn btn-primary"
+      to="/meetings"
+      v-if="user"
+    >Meetings</router-link>
+    
+
+    </div>
+
   </div>
 </template>
 
 <script>
-
+export default {
+  name: "home",
+  props: ["user"]
+};
 </script>
